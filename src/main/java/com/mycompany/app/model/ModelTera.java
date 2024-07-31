00208ca26165e9eab0021f73854ba0c686c0bafa
long.phanhoang <long.phahoang@teragroup.io>
2024-07-31 10:49:09 +0700

    Setup model

---
+++ src/main/java/com/mycompany/app/model/ModelTera.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.model;
+
+public class ModelTera {
+    private String name;
+
+    public ModelTera(String name) {
+        this.name = name;
+    }
+
+    public ModelTera() {
+    }
+}
